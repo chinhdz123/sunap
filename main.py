09b3bb9034ed23379b1348a75e4ea28bc421a013
Lê Ngọc Chinh <chinh.ln1820@gmail.com>
2022-11-27 11:09:01 +0700

    update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ warnings.filterwarnings("ignore")
 from utils import *
 
 
-img = cv2.imread(r"data\sunap3.bmp")
+img = cv2.imread(r"data\s1.bmp")
+# img = cv2.rotate(img,90)
 # bước 1: tìm vị trí sunap trên ảnh
 # h,w = img.shape[:2]
 # h1,w1 = int(h/2), int(w/2)
@@ -14,8 +15,8 @@ img = cv2.imread(r"data\sunap3.bmp")
 # img1 = img[:h1,:]
 # img2 = img[h1:h,:]
 
-x,y = find_circles(img)
-
+circles = find_total_circles(img)
+print(len(circles))
 # x2,y2 = find_circles(img2)
 # cv2.imwrite("tmp/img1.jpg",img1)
 # cv2.imwrite("tmp/img2.jpg",img2)
@@ -24,10 +25,10 @@ x,y = find_circles(img)
 # bước 2: convert vị trí bằng model
 # x = [ 519, 644, 775, 908, 511, 644, 779, 922, 507, 644, 785, 932, 499, 643, 793, 947, 486, 639, 799, 964, 476, 635, 804, 980]
 # y = [ 993, 988, 980, 965, 1107, 1101, 1095, 1081, 1231, 1230, 1221, 1209, 1367, 1364, 1357, 1349, 1513, 1510, 1505, 1502, 1671, 1673, 1674, 1676]
-x_robot,y_robot = convertgit_to_x_y_robot(x,y)
+# x_robot,y_robot = convert_to_x_y_robot(x,y)
 
 # print("x_robot",x_robot)
 # print("y_robot",y_robot)
 # bước 3: điều khiển robot theo các vị trí
-control(x_robot,y_robot)
+# control(x_robot,y_robot)
 
--- utils.py
+++ utils.py
@@ -5,9 +5,11 @@ from detectron2.config import get_cfg
 from loguru import logger
 import cv2
 import warnings
+import numpy as np
+from copy import deepcopy
 warnings.filterwarnings("ignore")
 cfg = get_cfg()
-cfg.merge_from_file("output\config.yaml")
+cfg.merge_from_file(r"output\new_model\output.yaml")
 logger.info("start_load_md")
 predictor_circles = DefaultPredictor(cfg)
 logger.info("end_load_md")
@@ -30,37 +32,24 @@ def find_line_circles(circles):
     group_lines.append(group_line)
     return group_lines
 
-def find_circles(img):
+def find_circles_in_small_img(img):
+    # w, h = img.shape[:2]
     logger.info("start predict")
     output = predictor_circles(img)
     boxes = output["instances"].to("cpu").pred_boxes if output["instances"].to("cpu").has("pred_boxes") else None
     if boxes is not None:
         boxes = convert_boxes(boxes)
     circles= []
+    r_s = []
     for box in boxes:
         x0, y0, x1, y1 = box
         center_X =  int((x1+x0)/2)
         center_Y = int((y1+y0)/2)
         r = int((x1-x0)/2)
+        r_s.append(r)
         circles.append([center_X,center_Y,r])
-        circles = sorted(circles, key=lambda b: b[1])
-    print(len(circles))
-    group_lines = find_line_circles(circles)
-    count = 0
-    x = []
-    y = []
-    for group_line in group_lines:
-        group_line = sorted(group_line, key=lambda b: b[0])
-        for circle in group_line:
-            cv2.circle(img, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
-            cv2.circle(img, (circle[0], circle[1]), 2, (0, 0, 255), 1)
-            cv2.putText(img, str(count), (circle[0], circle[1]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
-            x.append(circle[0])
-            y.append(circle[1])
-            count +=1
-    # cv2.imwrite("tmp/sunap2.jpg",img)
-    logger.info("end predict")
-    return x, y
+    return circles
+
 def convert_to_x_y_robot(x,y):
     model_x = torch.load(r"model\model_x.pth")
     model_y = torch.load(r"model\model_y.pth")
@@ -84,3 +73,55 @@ def convert_to_x_y_robot(x,y):
     predicteds_Y = [math.floor(predicted_Y.item()*81) for predicted_Y in predicteds_Y]
 
     return predicteds_X,predicteds_Y
+
+def find_total_circles(img):
+    h,w= img.shape[:2]
+    img1 = np.ones_like(img)
+    img2 = np.ones_like(img)
+
+    img1[:,:int(5.2*w/10)] = img[:,:int(5.2*w/10)]
+    img2[:,int(4.8*w/10):] = img[:,int(4.8*w/10):]
+    circles1 = find_circles_in_small_img(img1)
+    # print(len(circles1))
+    # count1 =0 
+    # for circle in circles1:
+    #     cv2.circle(img1, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
+    #     cv2.circle(img1, (circle[0], circle[1]), 2, (0, 0, 255), 1)
+    #     cv2.putText(img1, str(count1), (circle[0], circle[1]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+    #     count1+=1
+    # cv2.imwrite("tmp/rs1.jpg",img1)
+    circles2 = find_circles_in_small_img(img2)
+    # count2 =0 
+    # print(len(circles2))
+    # for circle in circles2:
+    #     cv2.circle(img2, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
+    #     cv2.circle(img2, (circle[0], circle[1]), 2, (0, 0, 255), 1)
+    #     cv2.putText(img2, str(count2), (circle[0], circle[1]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+    #     count2+=1
+    # cv2.imwrite("tmp/rs2.jpg",img2)
+    circles = circles1 + circles2
+    new_circles = deepcopy(circles)
+    for circle1 in  circles1:
+        for circle2 in circles2:
+            if ((circle2[0]-circle1[0])**2+(circle2[1]-circle1[1])**2)**(1/2) < 0.8*max(circle2[2],circle1[2]):
+                if circle2[2] < circle1[2]:
+                    new_circles.remove(circle2)
+                else:
+                    new_circles.remove(circle1)
+
+    circles = sorted(new_circles, key=lambda b: b[1])
+    group_lines = find_line_circles(circles)
+    new_circle = []
+    count = 0
+    for group_line in group_lines:
+        group_line = sorted(group_line, key=lambda b: b[0])
+        for circle in group_line:
+            if count == 39 or count == 40:
+                cv2.circle(img, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
+                cv2.circle(img, (circle[0], circle[1]), 2, (0, 0, 255), 1)
+                cv2.putText(img, str(count), (circle[0], circle[1]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+            count +=1
+            new_circle.append(circle)
+    cv2.imwrite("tmp/result1.jpg",img)
+
+    return circles
